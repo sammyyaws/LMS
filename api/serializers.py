9c879bf79a300bb0d186bb78a67392f05b8be463
Engineer Sam <sammyyawsnana1@gmail.com>
2025-11-11 12:47:56 +0000

    created a serializer for the registration of users called the createuserserializer

--- api/serializers.py
+++ api/serializers.py
@@ -24,3 +24,33 @@ class UserSerializer(serializers.ModelSerializer):
             'date_modified',
             'activated_at',
         )
+
+
+class CreateUserSerialzer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True)                                                               
+    class Meta:
+        model=User
+        fields=[
+            'username',
+            'email',
+            'password'                                                      
+            ]
+        extra_kwargs={
+        'password':{'write_only':True}
+        }
+
+def validate(self,attrs):
+            if User.objects.filter(email=attrs['email']).exists():
+                raise serializers.ValidationError({"email":"Email already exists"})
+            if User.objects.filter(username=attrs['username']).exists():
+                raise serializers.ValidationError({'username':'Username has been taken'})
+            return attrs
+        
+def create(self,validated_data):
+            user=User(
+                username=validated_data['username'],
+                email=validated_data['email']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
