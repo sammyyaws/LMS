af5cb34835249e26b6ac2a28e3ae8ea5360d9b2b
Vic-Toria-A <vasamoah55@gmail.com>
2025-11-25 10:11:35 +0000

    Created models folder

--- api/models/models.py
+++ api/models/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+
 ############### users model extensions
 class userProfile(models.Model):
    user=models.OneToOneField(User,on_delete=models.CASCADE)
