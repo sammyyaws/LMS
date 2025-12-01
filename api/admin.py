1c4d21f9ca3f13184d280e58b6f48cb84f1436b9
Engineer Sam <sammyyawsnana1@gmail.com>
2025-12-01 21:05:13 +0000

    registered all the course models on the admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .Models.user_models import userProfile
-from .Models.course_models import courses
+from .Models.course_models import courses,grade,Quiz,Assignment,Submission,lessons,course_categories,progress
+from .Models.roles_models import Role
 # Register your models here.
 admin.site.register(userProfile)
 admin.site.register(courses)
+admin.site.register(grade)
+admin.site.register(Quiz)
+admin.site.register(Assignment)
+admin.site.register(Submission)
+admin.site.register(lessons)
+admin.site.register(course_categories)
+admin.site.register(progress)
+admin.site.register(Role)
