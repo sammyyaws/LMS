bdf189f5f99adfdebf29242acbef79cc424b45f3
jermieasare <jermieasare@gmail.com>
2025-12-10 09:57:55 -0200

    created the url for the quiz model

--- api/urls/course_urls.py
+++ api/urls/course_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ..views.course_views import CourseViewSet   # adjust based on your structure
+from ..views.course_views import CourseViewSet, QuizViewSet   # adjust based on your structure
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
-
+router.register(r'quizzes', QuizViewSet, basename='quizzes')
 urlpatterns = [
     path('', include(router.urls)),
 ]
